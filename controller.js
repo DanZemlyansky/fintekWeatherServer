5fed65d5580a0b8c1a22db7a984219dfc84b56bd
Dan Zemlyansky <danzemly000@gmail.com>
2024-08-05 16:53:32 +0300

    initial commit

---
+++ controller.js
@@ -0,0 +1,4 @@
+const getWeather = (req , res) => {
+
+    
+}; //fetch weather and then filter to relevant result
---
+++ index.js
@@ -0,0 +1,3 @@
+const express = require('express');
+
+const API_KEY = '3485b849ad8841fc9ba120916240508' // should be stored as env but public for the sake of sharing
